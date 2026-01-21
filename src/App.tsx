27349ebcb97535441db077be656baf1ca625d8cf
panovark <arkadiypanov.dev@gmail.com>
2026-01-21 21:49:32 +0100

    feat: set up initial project with Vite, TypeScript, SCSS, and React Compiler.

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <main>
+        <section className="section hero">
+          <div className="container">
+            <h1>OAK 4 D Camera</h1>
+            <p>Project setup</p>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
