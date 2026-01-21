c7bb1c781970f582ccc495f38b062b2fd31086a9
panovark <arkadiypanov.dev@gmail.com>
2026-01-21 22:19:34 +0100

    chore: configure absolute path aliases for improved import management.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [
@@ -9,4 +10,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
